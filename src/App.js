09be1d4461f6089286950104c4b987f0c9f1bfd0
islammd39e <islammdrahat308@gmail.comm>
2023-01-17 20:06:34 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react'
+
 
 function App() {
+  // const [shows , setShows] = useState("")
+  const [peoples, setPeople] = useState([])
+
+  useEffect(()=>{
+    fetch('http://localhost:5000/users')
+    .then(res => res.json())
+    .then(data => setPeople(data))
+  },[])
+
+
+
+  const handleSubmit =(e)=>{
+    e.preventDefault()
+    const form = e.target;
+    const name = form.username.value;
+    const password = form.password.value;
+    const result = {name, password};
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className="flex justify-center">
+    <form onSubmit={handleSubmit} className='mt-5'>
+    <input type="text" name='username' placeholder="username" className="input input-bordered w-full max-w-xs" />
+     <input type="password" name='password' placeholder="password" className="input input-bordered w-full max-w-xs my-5" />
+     <button className='btn btn-primary block mx'>Post</button>
+    </form>
+    </div>
+  <div className="text-center block">
+  {
+    peoples.map(p => <p key={p.id}>{p.email}</p>)
+   }
+   {/* <p>{shows}</p> */}
+   
+  </div>
     </div>
   );
 }
